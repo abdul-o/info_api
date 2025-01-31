72940ff7ef88564fa1971f859ed8635e08aec6f1
Abdul-o <abdullateefdauda01@gmail.com>
2025-01-31 16:50:55 +0100

    api_corrected

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+
+$age = [
+    "email" => "abdullateefdauda01@gmail.com", 
+    "current_datetime" => gmdate('Y-m-d\TH:i:s\Z'), 
+    "github_url" => "https://github.com/abdul-o/info_api.git", 
+];
+
+echo json_encode($age, JSON_PRETTY_PRINT);
+
+
+
+
+?>
