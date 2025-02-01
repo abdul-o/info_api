cc3597f81a7f8d167552e2f0d27f00bea380b593
Abdul-o <abdullateefdauda01@gmail.com>
2025-02-01 03:54:55 +0100

    response_code added

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $age = [
     "current_datetime" => gmdate('Y-m-d\TH:i:s\Z'), 
     "github_url" => "https://github.com/abdul-o/info_api.git", 
 ];
-
+http_response_code(200);
 echo json_encode($age, JSON_PRETTY_PRINT);
 
 
